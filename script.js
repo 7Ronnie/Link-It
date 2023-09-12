8263ceecf5f0d5c50610cc86529077bb655ac2b6
Kunal <kunalchoudhary619.kc@gmail.com>
2023-09-13 01:46:22 +0530

    Links rendered

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const saveButton = document.getElementById("savebtn");
 
+let myleads = [];
+let inputEl = document.getElementById("input-el");
+let list = document.getElementById("list");
+
 saveButton.addEventListener("click",function () {
-    return alert("Hey User")}
-    );
+    myleads.push(inputEl.value)
+    
+    inputEl.innerHTML = "";    
+    renderleads();
+});
+
+function renderleads() {
+myleads.map((x) => {
+    let listitem = document.createElement("li");
+    listitem.innerHTML = x;
+    
+    list.appendChild(listitem);
+})
+}
+
+
